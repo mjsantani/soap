36a123393a4b92d31b235d69a1f64e004ced77d7
mayur.santani <mayur.santani@accenture.com>
2017-11-03 00:18:13 -0500

    First commit. Hello World!

---
+++ src/main/java/com/example/soap/demo/HelloEndpoint.java
@@ -0,0 +1,23 @@
+package com.example.soap.demo;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.GetHelloRequest;
+import io.spring.guides.gs_producing_web_service.GetHelloResponse;
+
+@Endpoint
+public class HelloEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getHelloRequest")
+    @ResponsePayload
+    public GetHelloResponse getHello(@RequestPayload GetHelloRequest request) {
+        GetHelloResponse response = new GetHelloResponse();
+        response.setGreetingresponse(request.getGreetingrequest());
+
+        return response;
+    }
+}
